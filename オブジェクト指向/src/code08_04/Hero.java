8a1530b6bd368d84b5a5b098bb064326560d4dee
nachi739 <namiki081025@gmail.com>
2021-09-29 18:20:20 +0900

    code08_04

---
+++ オブジェクト指向/src/code08_04/Hero.java
@@ -0,0 +1,7 @@
+package code08_04;
+
+public class Hero {
+	//フィールドを追加
+	String name;		//名前の宣言
+	int hp;				//HPの宣言
+}
---
+++ オブジェクト指向/src/code08_04/Main.java
@@ -0,0 +1,14 @@
+package code08_04;
+
+public class Main {
+
+	public static void main(String[] args) {
+		/* (以下の内容をJavaで記述していく)
+			勇者よ、この世界に生まれよ！
+			お化けキノコよ、この世界に生まれよ！
+			勇者よ、戦え！
+			お化けキノコよ、逃げろ！
+		*/
+	}
+
+}
