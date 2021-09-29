2912afff31a4c6494d0cc0687972ca68437deb7a
nachi739 <namiki081025@gmail.com>
2021-09-29 18:24:45 +0900

    code08_05

---
+++ オブジェクト指向/src/code08_05/Hero.java
@@ -0,0 +1,7 @@
+package code08_05;
+
+public class Hero {
+	//フィールドを追加
+	String name;		//名前の宣言
+	int hp;				//HPの宣言
+}
---
+++ オブジェクト指向/src/code08_05/Matango.java
@@ -0,0 +1,7 @@
+package code08_05;
+
+public class Matango {
+	//フィールドを追加
+	int hp;							//HPの宣言
+	final int LEVEL = 10;		//levelを定数で固定し宣言
+}
