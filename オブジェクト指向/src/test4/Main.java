27ad268f3b04e3285dea72876bd08f86a972af92
nachi739 <namiki081025@gmail.com>
2021-10-05 17:54:21 +0900

    test4

---
+++ オブジェクト指向/src/test4/Main.java
@@ -0,0 +1,13 @@
+package test4;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.nextLine();
+		System.out.println(s);
+
+		sc.close();
+	}
+}
